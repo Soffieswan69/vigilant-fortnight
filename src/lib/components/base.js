60fad411cb3746846a77bf99318c1b3f96533f99
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 18:03:49 +0800

    Initialize devnook-courses

---
+++ src/lib/components/base.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview Web Components used by all pages within web.dev.
+ */
+
+import 'webdev-infra/web-components/YouTube';
+
+import './ProfileSwitcherContainer';
+import './Header';
+import './LanguageSelect';
+import './NavigationDrawer';
+import './SnackbarContainer';
+import './Search';
+import './SearchResults';
