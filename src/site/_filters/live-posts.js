f359739d517d310f3cdc891b82615f9d6a29dc51
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 17:25:56 +0800

    Initialize future-posts

---
+++ src/site/_filters/live-posts.js
@@ -0,0 +1,20 @@
+const {env} = require("../_data/site");
+
+/**
+ * Filter draft posts out from a collection.
+ * @param {object} post An eleventy post object.
+ * @return {boolean} Whether or not the post should go live.
+ */
+module.exports = function livePosts(post) {
+  const now = new Date();
+  if (post.date > now) {
+    post.data.draft = true;
+  }
+
+  // If we're in dev mode, force draft posts to show up.
+  if (env === "dev") {
+    return true;
+  }
+
+  return !post.data.draft;
+};
