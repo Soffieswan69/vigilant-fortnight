dab71e3451a8c454089063c37bb647011467eb02
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 17:31:47 +0800

    Initialize webdev-live-share

---
+++ src/lib/pages/live.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoveriew Entrypoint for web.dev LIVE page.
+ */
+
+import '../components/EventShare';
+import '../components/Subscribe';
+import '../components/Tabs';
+
+import '../components/base';
