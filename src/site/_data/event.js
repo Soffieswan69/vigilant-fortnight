bc669351dd7a628f2185850fc589d4057bb4086f
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 17:32:14 +0800

    Initialize bug/improve-do-not-merge

---
+++ src/site/_data/event.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Contains event data for web.dev/LIVE.
+ */
+
+module.exports = [
+  {
+    title: 'Day 1',
+    when: '2020-06-30T16:00Z',
+    duration: 8,
+    sessions: [
+      {
+        speaker: 'addyosmani',
+        title: 'How to optimize for Web Vitals',
+      },
+      {
+        speaker: 'rviscomi',
+        title: 'How to browse CrUX data on BigQuery',
+      },
+    ],
+  },
+  {
+    title: 'Day 2',
+    when: '2020-07-01T16:00Z',
+    duration: 8,
+    sessions: [
+      {
+        speaker: 'samthor',
+        title: 'App-Like UX for Better PWAs',
+      },
+    ],
+  },
+  {
+    title: 'Day 3',
+    when: '2020-07-02T16:00Z',
+    duration: 8,
+    sessions: [],
+  },
+];
---
+++ src/site/_includes/components/Author.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {html} = require('common-tags');
+const AuthorInfo = require('./AuthorInfo');
+const contributors = require('../../_data/contributors');
+
+module.exports = ({id, showSocialMedia = false, small = false}) => {
+  const author = contributors[id];
+  if (!author) {
+    throw new Error(
+      `Can't create Author component for "${id}" without contributor ` +
+        `information. Please check '_data/contributors.js' and make sure the ` +
+        `author you provide is a key in this object.`,
+    );
+  }
+
+  if (!author.name) {
+    throw new Error(
+      `Can't create Author with missing author.name. author object: ` +
+        JSON.stringify(author),
+    );
+  }
+
+  return html`
+    <div class="w-author">
+      <a href="/authors/${id}">
+        <img
+          class="w-author__image ${small && `w-author__image--small`}"
+          src="/images/authors/${id}.jpg"
+          alt="${author.title}"
+        />
+      </a>
+      ${AuthorInfo({author, id, showSocialMedia})}
+    </div>
+  `;
+};
