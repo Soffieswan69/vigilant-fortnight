e2d5d1fc5d83e8b6ace5f587940f5136a7a2f155
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 17:51:23 +0800

    Initialize gitlocalize-13466

---
+++ src/site/_includes/components/Compare.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const {html} = require('common-tags');
+
+/* eslint-disable require-jsdoc */
+
+module.exports = (content, type, labelOverride) => {
+  if (!type) {
+    throw new Error(
+      "Can't create Compare component without a type. Did you forget to pass the type as a string?",
+    );
+  }
+
+  let label = labelOverride || '';
+  if (!label) {
+    switch (type) {
+      case 'worse':
+        label = "Don't";
+        break;
+
+      case 'better':
+        label = 'Do';
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return html`
+    <figure class="w-compare">
+      <p class="w-compare__label w-compare__label--${type}">
+        ${label}
+      </p>
+      ${content}
+    </figure>
+  `;
+};
