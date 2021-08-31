a2f0e8b103866d9864e6b7014b6b260e17d0aff6
Soffieswan69 <89755673+Soffieswan69@users.noreply.github.com>
2021-08-31 17:28:46 +0800

    Initialize add-question-feedback

---
+++ src/lib/pages/default.js
@@ -0,0 +1,16 @@
+/**
+ * @fileoveriew Default entrypoint for all pages.
+ */
+
+import "../components/Actions";
+import "../components/AssessmentFeedbackContainer";
+import "../components/AssessmentFeedbackForm";
+import "../components/AssessmentQuestion";
+import "../components/Codelab";
+import "../components/Assessment";
+import "../components/ResponseMultipleChoice";
+import "../components/SelectGroup";
+import "../components/Tabs";
+import "../components/CopyCode";
+
+import "../app";
